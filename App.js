256da9ffc2ef8c39f7fbf7100824f58ced65aff8
QuintonEL <quintonlaborde@gmail.com>
2020-05-19 15:31:53 -0600

    played with platform specific code regarding top status bar height

--- App.js
+++ App.js
@@ -1,28 +1,27 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Alert, Button } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Alert,
+  Button,
+  StatusBar,
+  Platform,
+} from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("text pressed");
 
   return (
-    <SafeAreaView style={[styles.container, containerStyle]}>
-      <Button
-        title="Click Me"
-        onPress={() =>
-          alert('Hi')
-        }
-      />
+    <SafeAreaView style={styles.container}>
+      <Button title="Click Me" onPress={() => alert("Hi")} />
     </SafeAreaView>
   );
 }
-
-const containerStyle = { backgroundColor: "orange"};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
