f51ee3b5cd04e3a57520d0987bda2b50174df4b2
QuintonEL <quintonlaborde@gmail.com>
2020-05-19 17:41:09 -0600

    created file structure for actual app, added in skeleton code for home page

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          flex: 1,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          flex: 1,
-        }}
-      />      
-      <View
-        style={{
-          backgroundColor: "tomato",
-          flex: 1,
-        }}
-      />
-    </View>
+    <WelcomeScreen />;
   );
 }
