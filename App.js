58dfa8262267127abc49e98aaa7c605a950bd802
QuintonEL <quintonlaborde@gmail.com>
2020-05-20 13:58:02 -0600

    added platform specific styling code for android and ios

--- App.js
+++ App.js
@@ -4,5 +4,5 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <WelcomeScreen />;
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -5,10 +5,21 @@ function AppText(props) {
   return <Text style={styles.text}>{props.chilren}</Text>;
 }
 
+
+
 const styles = StyleSheet.create({
   text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    color: 'tomato',
+    ...Platform.select({
+      ios: {
+        fontSize: 20,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 18,
+        fontFamily: "Roboto",
+      },
+    });
   },
 });
 
