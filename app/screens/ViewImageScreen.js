e231b298f8a8794b0f8db588d79a0fd02026f48b
QuintonEL <quintonlaborde@gmail.com>
2020-05-20 11:46:42 -0600

    refactored colors

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
 
+import colors from "../config/colors";
+
 function ViewImageScreen(props) {
   return (
     <View style={styles.container}>
@@ -19,21 +21,21 @@ const styles = StyleSheet.create({
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
-    position: 'absolute',
-    top: 40, 
+    backgroundColor: colors.primary,
+    position: "absolute",
+    top: 40,
     left: 30,
   },
   container: {
-    backgroundColor: "#000",
+    backgroundColor: colors.black,
     flex: 1,
   },
   deletecon: {
     width: 50,
     height: 50,
-    backgroundColor: "#4ecdc4",
-    position: 'absolute',
-    top: 40, 
+    backgroundColor: colors.secondary,
+    position: "absolute",
+    top: 40,
     right: 30,
   },
   image: {
