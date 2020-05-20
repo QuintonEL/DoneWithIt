5623c221c43924d7a2a0fb3ba90fdf3dfcaf68e8
QuintonEL <quintonlaborde@gmail.com>
2020-05-20 16:36:52 -0600

    card component has been built

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 
-import colors from "../config/colors";
 import AppText from "./AppText";
+import colors from "../config/colors";
 
 function Card({ title, subTitle, image }) {
   return (
     <View style={styles.card}>
-      <Image source={image} />
-      <AppText>{title}</AppText>
-      <AppText>{subTitle}</AppText>
+      <Image style={styles.image} source={image} />
+      <View style={styles.detailsContainer}>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subTitle}</AppText>
+      </View>
     </View>
   );
 }
@@ -19,6 +21,21 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     backgroundColor: colors.white,
     marginBottom: 20,
+    overflow: "hidden",
+  },
+  detailsContainer: {
+    padding: 20,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  subTitle: {
+    color: colors.secondary,
+    fontWeight: "bold",
+  },
+  title: {
+    marginBottom: 7,
   },
 });
 
