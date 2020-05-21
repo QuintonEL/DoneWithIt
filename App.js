c02b8bef0e7c3e086b14fc3fbac48e3e41b78ba9
QuintonEL <quintonlaborde@gmail.com>
2020-05-21 14:09:32 -0600

    added a messages screen component along with another component to deal with the status bar height on android and ios

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
