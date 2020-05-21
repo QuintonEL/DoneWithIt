a69f9c8a42290af6467885af4a656bd3c23f371c
QuintonEL <quintonlaborde@gmail.com>
2020-05-21 14:38:23 -0600

    added functionality to handle selection of messages

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -31,6 +31,7 @@ function MessagesScreen(props) {
             title={item.title}
             subTitle={item.description}
             image={item.image}
+            onPress={() => console.log("Message Selected", item)}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
