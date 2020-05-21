9c4a6df6221b1d118c251fa6b8b237be94c375d6
QuintonEL <quintonlaborde@gmail.com>
2020-05-21 14:26:21 -0600

    added new component for separator between comment sections

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import ListItemSeparator from "../components/ListItemSeparator";
 
 const messages = [
   {
@@ -32,13 +33,12 @@ function MessagesScreen(props) {
             image={item.image}
           />
         )}
+        ItemSeparatorComponent={ListItemSeparator}
       />
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default MessagesScreen;
