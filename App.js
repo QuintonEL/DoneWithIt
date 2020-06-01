817bbc6c5293c690ef3df3acb8d8c67037c72aaa
QuintonEL <quintonlaborde@gmail.com>
2020-06-01 15:12:55 -0600

    created a register screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
--- app/components/forms/SubmitButton.js
+++ app/components/forms/SubmitButton.js
@@ -6,7 +6,7 @@ import AppButton from "../AppButton";
 function SubmitButton({ title }) {
   const { handleSubmit } = useFormikContext();
 
-  return <AppButton title="Login" onPress={handleSubmit} />;
+  return <AppButton title={title} onPress={handleSubmit} />;
 }
 
 export default SubmitButton;
