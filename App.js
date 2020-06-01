58b94e77fdf547520f625962470a8c5139b2cb8f
QuintonEL <quintonlaborde@gmail.com>
2020-06-01 15:48:52 -0600

    built the listing edit screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import RegisterScreen from "./app/screens/RegisterScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <RegisterScreen />;
+  return <ListingEditScreen />;
 }
