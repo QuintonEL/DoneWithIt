e0d856d7afc8c1deca8c9bd0d66074fc3624edf4
QuintonEL <quintonlaborde@gmail.com>
2020-06-01 12:38:07 -0600

    built a error message component and refactored

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { StyleSheet, Image } from "react-native";
-import { Formik, yupToFormErrors } from "formik";
+import { Formik } from "formik";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
 import AppText from "../components/AppText";
 import AppTextInput from "../components/AppTextInput";
 import AppButton from "../components/AppButton";
+import ErrorMessage from "../components/ErrorMessage";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -33,7 +34,7 @@ function LoginScreen(props) {
               placeholder="Email"
               textContentType="emailAddress"
             />
-            <AppText style={{ color: "red" }}>{errors.email}</AppText>
+            <ErrorMessage error={errors.email} />
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -43,8 +44,7 @@ function LoginScreen(props) {
               secureTextEntry
               textContentType="password"
             />
-            <AppText style={{ color: "red" }}>{errors.password}</AppText>
-
+            <ErrorMessage error={errors.password} />
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
