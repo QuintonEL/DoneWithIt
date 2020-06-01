24487be3440de071ef6766abcd1897d74c8e3107
QuintonEL <quintonlaborde@gmail.com>
2020-06-01 14:59:00 -0600

    refactored and organized components

--- app/components/forms/AppForm.js
+++ app/components/forms/AppForm.js
@@ -9,7 +9,7 @@ function AppForm({ initialValues, onSubmit, validationSchema, children }) {
       onSubmit={onSubmit}
       validationSchema={validationSchema}
     >
-      {() => <>{{ children }}</>}
+      {() => <>{children}</>}
     </Formik>
   );
 }
