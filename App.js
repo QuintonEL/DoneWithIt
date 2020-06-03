8fe84ea2789393de61081130fd15db069d0509cc
QuintonEL <quintonlaborde@gmail.com>
2020-06-03 10:16:33 -0600

    added chevrons to the messages list page

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ListItem from "./app/components/lists/ListItem";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  return <MessagesScreen />;
 }
--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  TouchableHighlight,
-} from "react-native";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Swipeable from "react-native-gesture-handler/Swipeable";
 
 import AppText from "../AppText";
@@ -29,6 +24,11 @@ function ListItem({
             <AppText style={styles.title}>{title}</AppText>
             {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="chevron-right"
+            size={25}
+          />
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -37,11 +37,13 @@ function ListItem({
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flexDirection: "row",
     padding: 15,
     backgroundColor: colors.white,
   },
   detailsContainer: {
+    flex: 1,
     marginLeft: 10,
     justifyContent: "center",
   },
